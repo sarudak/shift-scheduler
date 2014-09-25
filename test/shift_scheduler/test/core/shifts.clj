3005b315d9674feae0236b4e949625bb9d9fea70
Seth Paulson <Sarudak@gmail.com>
2014-09-25 07:16:58 -0700

    Check for recurring shifts test

--- test/shift_scheduler/test/core/shifts.clj
+++ test/shift_scheduler/test/core/shifts.clj
@@ -10,6 +10,7 @@
    :recurrence-type :none
    })
 
+
 (def shift )
 
 (def shift-7to9-request shift-7to9)
@@ -22,18 +23,10 @@
                                           {:request-type :shifts
                                           :context-id :recurring-shifts
                                           :recurrence-type :weekly}])
-
 (def no-overlapping-request
   {:request shift-7to9-request
-   :context {:overlapping-shifts []}})
-
-(fact "When creating shifts if there are no overlapping shifts the shift is created"
-      (create-shift-script no-overlapping-request) => [{:command-type :create-shift
-                                                        :shift shift-7to9}])
-
-(def no-overlapping-request
-  {:request shift-7to9-request
-   :context {:overlapping-shifts []}})
+   :context {:overlapping-shifts []
+             :recurring-shifts []}})
 
 (def overlapping-shift
   {:start-time (date/date-time 2014 9 21 4 30)
@@ -45,15 +38,50 @@
   {:request shift-7to9-request
    :context {:overlapping-shifts [overlapping-shift]}})
 
+(def create-7to9-shift-response [{:command-type :create-shift
+                                  :shift shift-7to9}])
+
+(def create-shift-error-response [{:command-type :error
+                                   :message "Cannot create overlapping shifts"}])
+
+(fact "When creating shifts if there are no overlapping shifts the shift is created"
+      (create-shift-script no-overlapping-request) => create-7to9-shift-response
+      (create-shift-script overlapping-request) => create-shift-error-response)
+
+
+
+
+(fact "When creating shifts if there are overlapping shifts the shift is not created"
+      )
+
 (def overlapping-recurring-shift
   {:start-time (date/date-time 2014 9 14 4 30)
    :end-time (date/date-time 2014 9 14 5 30)
    :recurrence-type :weekly
    })
 
-(fact "When creating shifts if there are overlapping shifts the shift is not created"
-      (create-shift-script overlapping-request) => [{:command-type :error
-                                                     :message "Cannot create overlapping shifts"}])
+(def overlapping-recurring-request
+  {:request shift-7to9-request
+   :context {:overlapping-shifts []
+             :recurring-shifts [overlapping-recurring-shift]}})
+
+(def non-overlapping-recurring-shift
+  {:start-time (date/date-time 2014 9 14 4 30)
+   :end-time (date/date-time 2014 9 14 5 30)
+   :recurrence-type :weekly
+   })
+
+(def non-overlapping-recurring-request
+  {:request shift-7to9-request
+   :context {:overlapping-shifts []
+             :recurring-shifts [overlapping-recurring-shift]}})
+
+(fact "When creating shifts if there are recurrings shifts that overlap the shift is not created"
+      (create-shift-script overlapping-recurring-request) => create-shift-error-response
+      (create-shift-script non-overlapping-recurring-request) => create-7to9-shift-response)
+
+
+
 
 
 
