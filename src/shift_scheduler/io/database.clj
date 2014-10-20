5707c1d7ddeb550ca5f955b2eb7a33c94b59ce46
Seth Paulson <Sarudak@gmail.com>
2014-10-20 11:05:47 -0700

    Working commit

--- src/shift_scheduler/io/database.clj
+++ src/shift_scheduler/io/database.clj
@@ -1,6 +1,7 @@
 (ns shift-scheduler.io.database
   (:require [shift-scheduler.io.io :refer [query command]]
             [clojure.java.jdbc :as j]
+            [honeysql.core :as sql]
             [clj-time.core :as date]
             [shift-scheduler.core.date :refer [timestamp-to-date date-to-timestamp]]
             [shift-scheduler.io.db-convert :as db-convert]))
@@ -12,10 +13,15 @@
                :user "test"
                :password "test"})
 
-
+(defn query-from [request]
+  (sql/select :*)
+  (sql/from (:request-type request)
+  ))
 
 (defmethod query :shifts [request]
-  ())
+  (->>
+   (j/query mysql-db [(query-from request)])
+   (map db-convert/out)))
 
 (j/insert! mysql-db :shifts
   {:start_time (date-to-timestamp (date/date-time 2014 9 21 4))
@@ -23,17 +29,15 @@
    :recurrence_type 0})
 ;; ({:generated_key 1} {:generated_key 2})
 
-(def a-shift (first (j/query mysql-db
-  ["select * from shifts"])))
+(query {:request-type :shifts})
 
-
-(db-convert/out a-shift)
-(class (:start_time a-shift))
-
-(type (date/date-time 2014 9 21 4))
-
-(date-convert/from-long (.getTime (:end_time a-shift)))
-
-
-(s/replace "some_thing" "_" "-")
+(def get-shifts-request {:request-type :shifts
+  :context-id :overlapping-shifts
+  :where [:and
+          [:<= :end-time (date/date-time 2014 9 21 4)]
+          [:>= :start-time (date/date-time 2014 9 21 5)]]})
+(def get-recurring-shifts-request {:request-type :shifts
+  :context-id :recurring-shifts
+  :where [:= :recurrence-type :weekly]
+  })
 
